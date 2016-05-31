7821c2e4f91135c3ec298e9c5fb409fb3f335591
nxaxg <nxayancanxg@gmail.com>
2016-05-30 23:44:57 -0400

    VIII
    Desktop done.

--- tandoor/js/hidden-fig.js
+++ tandoor/js/hidden-fig.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     //hidden-fig
     $(".hidden-fig").hide();
+    $(".hidden-comida").hide();
+    $(".hidden-bebida").hide();
     
     
    $(".ver-mas-btn").click(function (e) {
@@ -9,6 +11,20 @@ $(document).ready(function(){
             $(".cont-menu .ver-mas-btn").html("No hay más platos para mostrar");
             $(".cont-vinos .ver-mas-btn").html("No hay más vinos para mostrar");
             $(".ver-mas-btn").addClass("pressed-btn");
+            e.preventDefault();
+        }
+    });
+    
+    $(".mas-comida-btn").click(function (e) {
+        $(".hidden-comida:hidden").slice(0, 4).fadeIn(500);
+        if ($(".hidden-comida:hidden").length === 0) {
+            e.preventDefault();
+        }
+    }); 
+    
+    $(".mas-bebida-btn").click(function (e) {
+        $(".hidden-bebida:hidden").slice(0, 4).fadeIn(500);
+        if ($(".hidden-bebida:hidden").length === 0) {
             e.preventDefault();
         }
     }); 
